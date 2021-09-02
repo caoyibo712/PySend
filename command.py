d70742aad0bcd86239fcaae349f580dbdf4e19b0
caoyibo712 <64115764+caoyibo712@users.noreply.github.com>
2021-09-02 17:53:00 +0800

    Update command.py

--- command.py
+++ command.py
@@ -1,9 +1,11 @@
 import cmd
 class fileCmd(cmd.Cmd):
+    #OK P
     intro='''Please Enter send [filepath] To Send A File,
     Then you will get A KEY TO get the file on another computer.
-    And ENTER get [key&path] To Get A File Which The key Means.'''.
+    And ENTER get [key&path] To Get A File Which The key Means.'''
     
+    #OK P
     def do_help(self, arg):
         return print(self.intro)
         
@@ -11,7 +13,7 @@ class fileCmd(cmd.Cmd):
         print(send(arg))
 
     def do_get(self,arg):
-        get(arg.split('&'))
+        get(arg)
         print('OK')
 
 if __name__=='__main__':
